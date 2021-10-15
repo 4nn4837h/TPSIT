1dbca3cae8d3b8d4a9b6d62296fdefdd743e85f2
4nn4837h <81015568+4nn4837h@users.noreply.github.com>
2021-10-15 10:14:05 +0200

    Add files via upload

---
+++ es_ripasso.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_LUNG 30      // manutenibilità del codice
+#define NUM 10
+/************************************************************************************************************************************
+es_01
+si scriva un programma in che:
+- definiscaun tipo di dato Studente. Ogni studente è caratterizzato da un nome, un cognome e una matricola.
+- acquisisca i dati di 10 studenti e, per ogni nuovo studente inserito, proceda ad un inserimento ordinato
+ (lo scopo dell'esercizio è ordinare gli sudenti durante il pricesso di inserimento degli stessi e
+  non dopo averli inseriti tutti con un algoritmo di ordinamento).
+- stampi i dati degli studenti.
+************************************************************************************************************************************/
+
+typedef char stringa[MAX_LUNG];
+
+typedef struct { // struttura campi 3 campi
+    stringa nome;
+    stringa cognome;
+    int matricola;
+} Studente;
+
+//void inserimentoDati(Studente c[]) {
+void stampa(Studente c[]){
+     for(int i = 0; i < NUM; i++) {
+        printf("\nNome: %s", c[i].nome);
+        printf("\nCognome: %s ", c[i].cognome);
+        printf("\nMatricola: %d", c[i].matricola);
+     }
+}
+int main() {
+    Studente classe[NUM]; // tabella di 10 righe con tipo di dato Studente
+    Studente tmp;
+    int i, j;
+
+    // acquisizione dei dati degli studenti -> standart input = tastiera
+    //inserimentoDati(classe);
+    for(int i = 0; i < NUM; i++) {
+        printf("Inserisci le informazioni di uno studente\nNome: ");
+        fflush(stdin);
+        scanf("%s", tmp.nome);
+        printf("Cognome: ");
+        fflush(stdin);
+        scanf("%s", tmp.cognome);
+        printf("Matricola: ");
+        fflush(stdin);
+        scanf("%d", &tmp.matricola);
+
+        // cerco la posizione di inserimento
+        while( i < NUM && classe[i].matricola < tmp.matricola) {
+            i += 1;
+            //creo lo spazio per inserire l'elemento
+            j = NUM;
+            while(j < i) {
+                tmp = classe[i];
+                classe[i] = classe[j - 1];
+                tmp = classe[i];
+            }
+        }
+    }
+
+    stampa(classe);
+
+    system("pause");
+    return 0;
+}
